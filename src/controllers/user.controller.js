5b07e2464a9f4c5ae8829a1f1015c7bac7947f98
JuanYa1901 <juan.yanez1901@alumnos.ubiobio.cl>
2024-10-06 21:12:17 -0300

    ADD update and Delete user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,7 +7,7 @@ export async function createUser(req, res) {
     
     try {
         
-        const userRepository = AppDataSource.getRepository(User)
+        const userRepository = AppDataSource.getRepository(User) //Instancia que permite interactuar con mi identidad usuario
 
         const user = req.body
 
@@ -98,4 +98,85 @@ export async function getUsers(req, res){
         console.error('Error al obtener un usuarios, el error: ', error)
     }
 
+}
+
+export async function updateUser(req, res) {
+
+    try {
+
+        const userRepository = AppDataSource.getRepository(User)
+        
+        const id = req.params.id
+        const user = req.body
+
+        const userFound = await userRepository.findOne({
+            where: [{
+                id:id
+            }]
+        })
+
+        if(!userFound){
+            return res.status(404).json({
+                message: "Este usuario no fue encontrado",
+                data: null
+            })
+        }
+
+            await userRepository.update(id,user)
+
+            const userData = await userRepository.findOne({
+                where:[{
+                    id:id
+                }]
+            })
+
+            res.status(200).json({
+                message: "Usuario actualizado correctamente gil :D",
+                data: userData
+            })
+    } catch (error) {
+        console.error("Error al actualizar un usuario: ", error)
+        res.status(500).json({message: "Error interno en el servidor"})
+    }
+    
+}
+
+export async function deleteUser(req, res) {
+
+    try {
+        const userRepository = AppDataSource.getRepository(User)
+
+        const id = req.params.id
+
+        const userFound = await userRepository.findOne({
+            where: [{
+                id:id
+            }]
+        })
+
+        if(!userFound){
+
+            return res.status(404).json({
+                message: "Usuario no encontrado",
+                data: null
+            })
+
+        }
+
+        const userDelete = await userRepository.remove(userFound)
+
+        res.status(200).json({
+            message: "Este gil se eliminó",
+            data: userDelete
+        })
+        
+
+
+    } catch (error) {
+
+        console.error("Error al eliminar el usuario: ", error)
+        res.status(500).json({message: "Error interno del servidor"})
+        
+    }
+    
 }
